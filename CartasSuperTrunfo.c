ca5fcec9aa4791530bfd816a4a1b4770a8945f9e
tafenes <92691048+tafenes@users.noreply.github.com>
2025-01-19 15:34:39 -0300

    Update CartasSuperTrunfo2.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,123 +1,15 @@
 #include <stdio.h>
 
+int main (){
 
+    char estadoA, estadoB, estadoC, estadoD, estadoE, estadoF, estadoG, estadoH;
+    char cidade01, cidade02, cidade03, cidade04;
+    float populacao01, populacao02, populacao03, populacao04;
+    float area01, area02, area03, area04;
+    float pib01, pib02, pib03, pib04;
+    int numero01, numero02, numero03, numero04;
 
-int main() {
-    
-    char estadoA [40];
-    char cidade01[40];
-    float populacao;
-    float area;
-    float pib;
-    int numero;
-
-    char cidade02[40];
-    float populacao02;
-    float area02;
-    float pib02;
-    int numero02;
-
-     char cidade03[40];
-    float populacao03;
-    float area03;
-    float pib03;
-    int numero03;
-
-    char cidade04[40];
-    float populacao04;
-    float area04;
-    float pib04;
-    int numero04;
-
-    printf("Seja bem-vindo ao Cartas Super Trunfo\n");
-    printf("Digite o nome da sua primeira cidade: \n");
-    scanf("%s", &cidade01);
-    
-    printf("População:\n");
-    scanf("%f", &populacao);
-
-    printf("Área: \n");
-    scanf("%f", &area);
-
-    printf("PIB: \n");
-    scanf("%f", &pib);
-
-    printf("Número de pontos turísticos: \n");
-    scanf("%d", &numero);
-
-
-
-
-     printf("Digite agora o nome da sua segunda cidade: \n");
-    scanf("%s", &cidade02);
-    
-    printf("População:\n");
-    scanf("%f", &populacao02);
-
-    printf("Área: \n");
-    scanf("%f", &area02);
-
-    printf("PIB: \n");
-    scanf("%f", &pib02);
-
-    printf("Número de pontos turísticos: \n");
-    scanf("%d", &numero02);
-
-
-
-
-     printf("Digite agora o nome da sua terceira cidade: \n");
-    scanf("%s", &cidade03);
-    
-    printf("População:\n");
-    scanf("%f", &populacao03);
-
-    printf("Área: \n");
-    scanf("%f", &area03);
-
-    printf("PIB: \n");
-    scanf("%f", &pib03);
-
-    printf("Número de pontos turísticos: \n");
-    scanf("%d", &numero03);
-
-
-
-    printf("Digite agora o nome da sua quarta cidade: \n");
-    scanf("%s", &cidade04);
-    
-    printf("População:\n");
-    scanf("%f", &populacao04);
-
-    printf("Área: \n");
-    scanf("%f", &area04);
-
-    printf("PIB: \n");
-    scanf("%f", &pib04); 
-
-    printf("Número de pontos turísticos: \n");
-    scanf("%d", &numero04);  
-    
-    printf("Cidade01: %s \n", cidade01);
-    printf("População: %f \n", populacao);
-    printf("Área: %f \n", area);
-    printf("Pib %f \n", pib);
-    printf("Número de pontos turísticos: %d \n \n", numero);
-    
-    printf("Cidade02: %s \n", cidade02);
-    printf("População: %f \n", populacao02);
-    printf("Área: %f \n", area02);
-    printf("Pib %f \n", pib02);
-    printf("Número de pontos turísticos: %d \n \n", numero02);
-    
-    printf("Cidade02: %s \n", cidade03);
-    printf("População: %f \n", populacao03);
-    printf("Área: %f \n", area03);
-    printf("Pib %f \n", pib03);
-    printf("Número de pontos turísticos: %d \n \n", numero03);
-     
-    printf("Cidade02: %s \n", cidade04);
-    printf("População: %f \n", populacao04);
-    printf("Área: %f \n", area04);
-    printf("Pib %f \n", pib04);
-    printf("Número de pontos turísticos: %d \n \n", numero04);}
+    printf("***Seja bem-vindo ao Super Trunfo Países***");
+    printf("Vamos começar cadastrando seu primeiro estado. (Cada estado deve conter 4 cidades));
+        
+}
